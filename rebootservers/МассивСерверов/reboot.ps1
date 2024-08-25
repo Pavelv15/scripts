b7c15df818f807a903ba84de479d1fe300766eba
Pavelv15 <pashokwolf15@mail.ru>
2024-08-25 17:31:09 +0300

    + скрипт по перезагрузке сервверов

---
+++ rebootservers/МассивСерверов/reboot.ps1
@@ -0,0 +1,38 @@
+﻿[array]$Servers= '' # Массив серверов
+$i = 1
+while($i = 1) {
+write-host "Выберите действие:"
+write-host "1-перезагрузка серверов"
+write-host "2-проверка после перезагрузки(после перезагрузки необходимо подождать минут 30)"
+write-host "3-Выход"
+$what=read-host " "
+if ( $what -eq "1") {
+for ($i=0; $i -lt $Servers.Length;$i++) {
+invoke-command -computername  $Servers[$i]   -scriptblock {
+( Get-WmiObject win32_computersystem).name
+shutdown /r /t 0 
+}
+
+}
+}
+
+if ( $what -eq "2") {
+for ($i=0; $i -lt $Servers.Length;$i++) {
+invoke-command -computername  $Servers[$i]   -scriptblock {
+( Get-WmiObject win32_computersystem).name
+
+Get-CimInstance -ClassName win32_OperatingSystem | select csname, lastbootuptime
+
+Write-host -BackgroundColor black -ForegroundColor Cyan "Внимательно изучи дату и время запуска ОС каждого сервера, если дата не совпадает с текущей, то необходимо руками перезапустить сервер"
+
+}
+}
+}
+if ( $what -eq "3")
+{
+exit
+}
+else {
+write-host -BackgroundColor red -ForegroundColor black "Неверный выбор"
+}
+}
---
+++ rebootservers/СписокСерверовИзЭксельФайла/reboot.ps1
@@ -0,0 +1,44 @@
+﻿
+$i = 1
+while($i = 1) {
+write-host "Выберите действие:"
+write-host "1-перезагрузка серверов"
+write-host "2-проверка после перезагрузки(после перезагрузки необходимо подождать минут 30)"
+write-host "3-Выход"
+$what=read-host " "
+if ( $what -eq "1") {
+$SRVNm = Get-Content .\name.csv 
+ForEach( $SRV in $SRVNm )  {
+invoke-command -computername  $SRV   -scriptblock {
+( Get-WmiObject win32_computersystem).name
+shutdown /r /t 0 
+}
+
+}
+}
+
+if ( $what -eq "2") {
+$SRVNm = Get-Content .\name.csv
+$data = Get-Date -Format dd_MM_yy
+write-host "Результат будет в файле result_reboot$($data).txt"
+$result = ForEach( $SRV in $SRVNm ) {
+
+invoke-command -computername  $SRV   -scriptblock {
+ ( Get-WmiObject win32_computersystem).name
+
+Get-CimInstance -ClassName win32_OperatingSystem | select csname, lastbootuptime
+
+echo "Внимательно изучи дату и время запуска ОС каждого сервера, если дата не совпадает с текущей, то необходимо руками перезапустить сервер"
+
+}
+}
+$result | Out-File result_reboot$($data).txt -Append;
+}
+if ( $what -eq "3")
+{
+exit
+}
+else {
+write-host -BackgroundColor red -ForegroundColor black "Неверный выбор"
+}
+}
