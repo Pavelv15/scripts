5ba55356881af3de9df82e7ba26c71c849ae55e8
Pavelv15 <102380069+Pavelv15@users.noreply.github.com>
2024-09-20 10:25:05 +0300

    Update run.bat
    + Комментарий

--- Napominalka/run.bat
+++ Napominalka/run.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -nologo -noninteractive -WindowStyle hidden -command .\nap.ps1
+powershell -nologo -noninteractive -WindowStyle hidden -command .\nap.ps1 :: Тут лучше указать полный путь до скрипта nap.ps1
